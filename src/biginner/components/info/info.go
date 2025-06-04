24ee5ad222e82d562b6978f4c82fdabb908b0612
JohnnelBaylen <jnelbaylen@gmail.com>
2025-06-05 00:45:36 +0800

    chore: update file structure

---
+++ src/biginner/components/info/info.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 19
+	var fullName string = "John Doe"
+	var height float32 = 5.8
+	var isDev bool = true
+
+	fmt.Println("Info : ")
+	fmt.Println()
+
+	fmt.Println("Name:", fullName)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is Developer:", isDev)
+}
